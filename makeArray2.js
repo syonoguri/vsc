d31eaf0220845a28209407da6cb07cd7dbe6dcb5
syonoguri <42812192+syonoguri@users.noreply.github.com>
2018-08-30 14:03:44 +0900

    Add files via upload

---
+++ makeArray2.js
@@ -0,0 +1,15 @@
+const makeArray2 = function() {
+    let x = [];
+    let i = 0;
+    (function push_i() {
+        i++;
+        if(i>30) { return; }
+        if(i%3!==0 && i%5!==0 && i%7!==0 && i%11!==0) { x.push(i); } 
+        push_i()       
+    })();
+    return x;
+}
+
+console.log(makeArray2());
+
+// 最初に配列を用意するバージョンを作る
